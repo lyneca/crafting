179a99fe967841dad6b5c5ffeee6bbcab1196d50
lyneca <lukemtuthill@gmail.com>
2020-01-01 14:49:06 +1100

    Updates

---
+++ precache-manifest.58f340235b3104fa29868e033f9582b8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d67d4e88a938c3135b9cb9a481fb3aa7",
+    "url": "/crafting/index.html"
+  },
+  {
+    "revision": "c14356045dea4172754f",
+    "url": "/crafting/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "5311550c37ce2e340b8c",
+    "url": "/crafting/static/js/2.60f6defb.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/crafting/static/js/2.60f6defb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c14356045dea4172754f",
+    "url": "/crafting/static/js/main.7e5cf425.chunk.js"
+  },
+  {
+    "revision": "51368d4bafa731574b2a",
+    "url": "/crafting/static/js/runtime-main.3984bc98.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/crafting/static/media/logo.5d5d9eef.svg"
+  }
+]);
